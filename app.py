65d5adb75aaa0bdb61f1731ca03f7994eb270a24
Ayan Abedin <ayanabedin@gmail.com>
2024-04-11 23:30:57 +0100

    Removed OCR Link

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
 from number_plate_test1 import my_function
-from ocr_regex import my_function_ocr
-import subprocess
+
 
 app = Flask(__name__)
 
-# Define the path to the virtual environment
-virtualenv_path = '/path/to/your/virtual/environment/bin/activate'
 
 # Define routes for signup, login, and index pages
 @app.route('/')
@@ -40,6 +37,7 @@ def park_lot():
     # Render the parking-slot.html template
     return render_template('parking-slot.html')
 
+
 @app.route('/ocr')
 def ocr():
     # Render the parking-slot.html template
@@ -51,11 +49,6 @@ def run_script():
     result = my_function()
     return result
 
-# Run the OCR code
-@app.route('/run_ocr')
-def run_ocr():
-    result = my_function_ocr()
-    return result
 
 
 if __name__ == '__main__':
